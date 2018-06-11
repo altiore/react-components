1e95d70b914a2850278746f205267dd6c9262dab
Razzwan <razvanlomov@gmail.com>
2018-06-12 01:08:57 +0300

    new build

--- build/Button/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Button as ButtonJsx } from './Button';
-export declare const Button: typeof ButtonJsx;
--- build/Button/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CSSModules = require("react-css-modules");
-var Button_1 = require("./Button");
-var styles = require("./style.m.scss");
-exports.Button = CSSModules(Button_1.Button, styles);
-//# sourceMappingURL=index.js.map
